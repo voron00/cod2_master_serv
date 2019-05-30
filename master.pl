471cd1b8c8b5184cb5f38744619f663f241a34d6
Alexey <alexey12424323@mail.ru>
2019-05-30 12:39:07 +0300

    Improve code formatting

--- master.pl
+++ master.pl
@@ -7,12 +7,12 @@ use threads;
 use IO::Socket;
 
 my $master_port = 20710;
-my $auth_port = 20700;
+my $auth_port   = 20700;
 
 my $msg;
 my $maxlen = 1024;
 
-my $debug = 0;
+my $debug         = 0;
 my $guid_hash_key = 'CD-KEY';
 
 my %server_list;
@@ -21,12 +21,12 @@ my %auth_list;
 &build_server_list;
 
 my $master_socket = IO::Socket::INET->new(LocalPort => $master_port, Type => SOCK_DGRAM, Proto => 'udp', Timeout => 5) or die "Socket: $@";
-my $auth_socket = IO::Socket::INET->new(LocalPort => $auth_port, Type => SOCK_DGRAM, Proto => 'udp', Timeout => 5) or die "Socket: $@";
+my $auth_socket   = IO::Socket::INET->new(LocalPort => $auth_port,   Type => SOCK_DGRAM, Proto => 'udp', Timeout => 5) or die "Socket: $@";
 
-my $auth_thread = async { &auth_server };
+my $auth_thread = async {&auth_server};
 $auth_thread->detach();
 
-my $info_thread = async { &update_servers_info };
+my $info_thread = async {&update_servers_info};
 $info_thread->detach();
 
 &master_server;
@@ -34,11 +34,11 @@ $info_thread->detach();
 sub update_servers_info {
 	my $line;
 	my $request_time = 120;
-	my $server_timeout = 1800;
+	my $timeout      = 1800;
 
 	while (1) {
 		foreach $line (keys %server_list) {
-			my $time = &get_time($server_list{$line});
+			my $time     = &get_time($server_list{$line});
 			my $protocol = &get_protocol($server_list{$line});
 
 			if ((time - $time) >= ($request_time)) {
@@ -51,7 +51,7 @@ sub update_servers_info {
 				sleep 1;
 			}
 
-			if ((time - $time) >= ($server_timeout)) {
+			if ((time - $time) >= ($timeout)) {
 				delete($server_list{$line});
 			}
 		}
@@ -82,24 +82,24 @@ sub get_protocol {
 
 sub request_server_info {
 	my $serverport = shift;
-    my $message = '';
-    my $info = ("\xFF\xFF\xFF\xFFgetinfo");
+	my $message    = '';
+	my $info       = ("\xFF\xFF\xFF\xFFgetinfo");
 
 	my $server;
 	my $port;
 
 	if ($serverport =~ /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\:(\d{1,5})$/) {
 		$server = $1;
-		$port = $2;
+		$port   = $2;
 	}
 	else {
 		die("request_server_info: Invalid serverport $serverport");
 	}
 
-    my $d_ip = inet_aton($server);
-    my $portaddr = sockaddr_in($port, $d_ip);
+	my $d_ip = inet_aton($server);
+	my $portaddr = sockaddr_in($port, $d_ip);
 
-    send($master_socket, $info, 0, $portaddr) == length($info) or die("Cannot send message");
+	send($master_socket, $info, 0, $portaddr) == length($info) or die("Cannot send message");
 }
 
 sub master_server {
@@ -111,7 +111,7 @@ sub master_server {
 		my $ip = inet_ntoa($ipaddr);
 
 		if ($debug) {
-			print "Master Server: client $ip:$port ($host) said $msg\n"; 
+			print "Master Server: client $ip:$port ($host) said $msg\n";
 		}
 
 		if ($msg =~ /^\xFF\xFF\xFF\xFFgetservers\s(\d+)\s?(\w+)?\s?(\w+)?$/) {
@@ -141,7 +141,7 @@ sub auth_server {
 		my $ip = inet_ntoa($ipaddr);
 
 		if ($debug) {
-			print "Authentication Server: client $ip:$port ($host) said $msg\n"; 
+			print "Authentication Server: client $ip:$port ($host) said $msg\n";
 		}
 
 		if ($msg =~ /^\xFF\xFF\xFF\xFFgetKeyAuthorize\s(\d)\s(\w+)$/) {
@@ -185,12 +185,12 @@ sub add_server {
 }
 
 sub update_server_info {
-	my $server = shift;
-	my $msg = shift;
+	my $server   = shift;
+	my $msg      = shift;
 	my $protocol = '0';
 
 	if (defined($server_list{$server})) {
-		if ($msg =~ /\\protocol\\(\d+)\\/) { $protocol = $1 }; 
+		if ($msg =~ /\\protocol\\(\d+)\\/) { $protocol = $1 }
 		$server_list{$server} = time . ";" . $protocol;
 	}
 
@@ -204,16 +204,16 @@ sub remove_server {
 }
 
 sub send_server_list {
-	my $adr = shift;
-	my $protocol = shift;
-	my $header = "\xFF\xFF\xFF\xFFgetserversResponse\n\x00";
+	my $adr       = shift;
+	my $protocol  = shift;
+	my $header    = "\xFF\xFF\xFF\xFFgetserversResponse\n\x00";
 	my $delimiter = "\\";
-	my $eot = "EOT";
-	my $eof = "EOF";
+	my $eot       = "EOT";
+	my $eof       = "EOF";
 	my $line;
-	my $max_per_packet = 20;
+	my $max_per_packet   = 20;
 	my $per_packet_count = 0;
-	my $data = $header . $delimiter;
+	my $data             = $header . $delimiter;
 	my $i;
 	my $server_protocol;
 
@@ -223,7 +223,7 @@ sub send_server_list {
 		next if ($server_protocol ne $protocol);
 
 		if ($line =~ /^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})\:(\d{1,5})$/) {
-			$data = $data . pack('C', $1); 
+			$data = $data . pack('C', $1);
 			$data = $data . pack('C', $2);
 			$data = $data . pack('C', $3);
 			$data = $data . pack('C', $4);
@@ -234,9 +234,9 @@ sub send_server_list {
 			if ($per_packet_count == $max_per_packet) {
 				$data = $data . $eot;
 				send($master_socket, $data, 0, $adr) == length($data) or die("Socket error: $!");
-				
+
 				$per_packet_count = 0;
-				$data = $header . $delimiter;
+				$data             = $header . $delimiter;
 			}
 		}
 	}
@@ -246,14 +246,14 @@ sub send_server_list {
 }
 
 sub xor_encode_int {
-    my ($str, $key) = @_;
-    my $enc_str = '';
+	my ($str, $key) = @_;
+	my $enc_str = '';
 
-    for my $char (split //, $str){
-        my $decode = chop $key;
-        $enc_str .= int(ord($char) ^ ord($decode));
-        $key = $decode . $key;
-    }
+	for my $char (split //, $str) {
+		my $decode = chop $key;
+		$enc_str .= int(ord($char) ^ ord($decode));
+		$key = $decode . $key;
+	}
 
-    return $enc_str;
+	return $enc_str;
 }
