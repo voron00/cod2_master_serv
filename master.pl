2133976cb2f309c65d7ef5162b5f537a132a71f7
Alexey <alexey12424323@mail.ru>
2019-05-30 12:32:19 +0300

    Create master.pl

---
+++ master.pl
@@ -0,0 +1,259 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use diagnostics;
+use threads;
+use IO::Socket;
+
+my $master_port = 20710;
+my $auth_port = 20700;
+
+my $msg;
+my $maxlen = 1024;
+
+my $debug = 0;
+my $guid_hash_key = 'CD-KEY';
+
+my %server_list;
+my %auth_list;
+
+&build_server_list;
+
+my $master_socket = IO::Socket::INET->new(LocalPort => $master_port, Type => SOCK_DGRAM, Proto => 'udp', Timeout => 5) or die "Socket: $@";
+my $auth_socket = IO::Socket::INET->new(LocalPort => $auth_port, Type => SOCK_DGRAM, Proto => 'udp', Timeout => 5) or die "Socket: $@";
+
+my $auth_thread = async { &auth_server };
+$auth_thread->detach();
+
+my $info_thread = async { &update_servers_info };
+$info_thread->detach();
+
+&master_server;
+
+sub update_servers_info {
+	my $line;
+	my $request_time = 120;
+	my $server_timeout = 1800;
+
+	while (1) {
+		foreach $line (keys %server_list) {
+			my $time = &get_time($server_list{$line});
+			my $protocol = &get_protocol($server_list{$line});
+
+			if ((time - $time) >= ($request_time)) {
+				&request_server_info($line);
+				sleep 1;
+			}
+
+			if ($protocol eq '0') {
+				&request_server_info($line);
+				sleep 1;
+			}
+
+			if ((time - $time) >= ($server_timeout)) {
+				delete($server_list{$line});
+			}
+		}
+
+		sleep 1;
+	}
+}
+
+sub get_time {
+	my $server = shift;
+
+	if ($server =~ /^(\d+);(\d+)$/) {
+		return $1;
+	}
+
+	return undef;
+}
+
+sub get_protocol {
+	my $server = shift;
+
+	if ($server =~ /^(\d+);(\d+)$/) {
+		return $2;
+	}
+
+	return undef;
+}
+
+sub request_server_info {
+	my $serverport = shift;
+    my $message = '';
+    my $info = ("\xFF\xFF\xFF\xFFgetinfo");
+
+	my $server;
+	my $port;
+
+	if ($serverport =~ /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\:(\d{1,5})$/) {
+		$server = $1;
+		$port = $2;
+	}
+	else {
+		die("request_server_info: Invalid serverport $serverport");
+	}
+
+    my $d_ip = inet_aton($server);
+    my $portaddr = sockaddr_in($port, $d_ip);
+
+    send($master_socket, $info, 0, $portaddr) == length($info) or die("Cannot send message");
+}
+
+sub master_server {
+	print "Starting master server on port $master_port\n";
+
+	while (my $adr = recv($master_socket, $msg, $maxlen, 0)) {
+		my ($port, $ipaddr) = sockaddr_in($adr);
+		my $host = gethostbyaddr($ipaddr, AF_INET);
+		my $ip = inet_ntoa($ipaddr);
+
+		if ($debug) {
+			print "Master Server: client $ip:$port ($host) said $msg\n"; 
+		}
+
+		if ($msg =~ /^\xFF\xFF\xFF\xFFgetservers\s(\d+)\s?(\w+)?\s?(\w+)?$/) {
+			&send_server_list($adr, $1);
+		}
+		elsif ($msg =~ /^\xFF\xFF\xFF\xFFheartbeat\sCOD-2$/) {
+			&add_server("$ip:$port");
+		}
+		elsif ($msg =~ /^\xFF\xFF\xFF\xFFinfoResponse\s(.*)$/) {
+			&update_server_info("$ip:$port", $1);
+		}
+		elsif ($msg =~ /^\xFF\xFF\xFF\xFFstatusResponse\s(.*)$/) {
+			&update_server_info("$ip:$port", $1);
+		}
+		elsif ($msg =~ /^\xFF\xFF\xFF\xFFheartbeat\sflatline$/) {
+			&remove_server("$ip:$port");
+		}
+	}
+}
+
+sub auth_server {
+	print "Starting authentication server on port $auth_port\n";
+
+	while (my $adr = recv($auth_socket, $msg, $maxlen, 0)) {
+		my ($port, $ipaddr) = sockaddr_in($adr);
+		my $host = gethostbyaddr($ipaddr, AF_INET);
+		my $ip = inet_ntoa($ipaddr);
+
+		if ($debug) {
+			print "Authentication Server: client $ip:$port ($host) said $msg\n"; 
+		}
+
+		if ($msg =~ /^\xFF\xFF\xFF\xFFgetKeyAuthorize\s(\d)\s(\w+)$/) {
+			$auth_list{$ip} = $2;
+		}
+		elsif ($msg =~ /^\xFF\xFF\xFF\xFFgetIpAuthorize\s(-?\d+)\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s(\w+)\s(\d)$/) {
+			my $guid = 0;
+
+			if (defined($auth_list{$2})) {
+				$guid = xor_encode_int($auth_list{$2}, $guid_hash_key);
+				$guid = substr($guid, -6);
+			}
+
+			my $data = "\xFF\xFF\xFF\xFFipAuthorize $1 accept ACCEPT $guid";
+			send($auth_socket, $data, 0, $adr) == length($data) or die("Socket error: $!");
+		}
+	}
+}
+
+sub build_server_list {
+	my $line;
+	my $list = "servers.txt";
+
+	if (-e $list) {
+		open(SERVER_LIST, $list) or die("unable to open $list: $!\n");
+
+		while (defined($line = <SERVER_LIST>)) {
+			$line =~ s/\s+$//;
+
+			if ($line =~ /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\:(\d{1,5})$/) {
+				$server_list{$line} = time . ";0";
+			}
+		}
+	}
+}
+
+sub add_server {
+	my $server = shift;
+	if (!defined($server_list{$server})) { $server_list{$server} = time . ";0"; }
+	if ($debug) { print "Add: $server\n"; }
+}
+
+sub update_server_info {
+	my $server = shift;
+	my $msg = shift;
+	my $protocol = '0';
+
+	if (defined($server_list{$server})) {
+		if ($msg =~ /\\protocol\\(\d+)\\/) { $protocol = $1 }; 
+		$server_list{$server} = time . ";" . $protocol;
+	}
+
+	if ($debug) { print "Update: $server Protocol: $protocol\n"; }
+}
+
+sub remove_server {
+	my $server = shift;
+	if (defined($server_list{$server})) { delete($server_list{$server}); }
+	if ($debug) { print "Remove: $server\n"; }
+}
+
+sub send_server_list {
+	my $adr = shift;
+	my $protocol = shift;
+	my $header = "\xFF\xFF\xFF\xFFgetserversResponse\n\x00";
+	my $delimiter = "\\";
+	my $eot = "EOT";
+	my $eof = "EOF";
+	my $line;
+	my $max_per_packet = 20;
+	my $per_packet_count = 0;
+	my $data = $header . $delimiter;
+	my $i;
+	my $server_protocol;
+
+	foreach $line (keys %server_list) {
+		$server_protocol = &get_protocol($server_list{$line});
+
+		next if ($server_protocol ne $protocol);
+
+		if ($line =~ /^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})\:(\d{1,5})$/) {
+			$data = $data . pack('C', $1); 
+			$data = $data . pack('C', $2);
+			$data = $data . pack('C', $3);
+			$data = $data . pack('C', $4);
+			$data = $data . pack('n', $5);
+			$data = $data . $delimiter;
+			$per_packet_count++;
+
+			if ($per_packet_count == $max_per_packet) {
+				$data = $data . $eot;
+				send($master_socket, $data, 0, $adr) == length($data) or die("Socket error: $!");
+				
+				$per_packet_count = 0;
+				$data = $header . $delimiter;
+			}
+		}
+	}
+
+	$data = $data . $eof;
+	send($master_socket, $data, 0, $adr) == length($data) or die("Socket error: $!");
+}
+
+sub xor_encode_int {
+    my ($str, $key) = @_;
+    my $enc_str = '';
+
+    for my $char (split //, $str){
+        my $decode = chop $key;
+        $enc_str .= int(ord($char) ^ ord($decode));
+        $key = $decode . $key;
+    }
+
+    return $enc_str;
+}
